3e645b5576bda32a3e03eb8c0ef22fbfa0d67770
alex82rm <alex82rm@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-02 14:24:44 +0000

    Aggiunta la classe (e solo la classe) della schermata iniziale per il primo avvio

---
+++ trunk/First Application/SchermataIniziale_primoAvvio.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace First_Application
+{
+	public partial class SchermataIniziale_primoAvvio : Form
+	{
+		public SchermataIniziale_primoAvvio()
+		{
+			InitializeComponent();
+		}
+
+		private void quit_menuItemAction(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void avanti_menuItemAction(object sender, EventArgs e)
+		{
+			if (controllaPassword())
+			{
+				MessageBox.Show("Le password corrispondono!"); //TODO controllare anche la creazione del file conf
+				//String s = Encrypt(password_textbox.Text);
+				//MessageBox.Show(s);
+				//s = Decrypt(s);
+				//MessageBox.Show(s);
+			}
+			else
+			{
+				MessageBox.Show("Attenzione, le password inserite non corrispondono");
+				password_textbox.Text = "";
+				ripeti_pwd_textbox.Text = "";
+				password_textbox.Focus();
+			}
+		}
+
+		private bool controllaPassword() {
+			if (password_textbox.Text != ripeti_pwd_textbox.Text)
+				return false;
+			else
+				return true;
+		}
+
+		//public static string Encrypt(string originalString)
+		//{
+		//  if (String.IsNullOrEmpty(originalString))
+		//  {
+		//    throw new ArgumentNullException
+		//           ("The string which needs to be encrypted can not be null.");
+		//  }
+		//  DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+		//  MemoryStream memoryStream = new MemoryStream();
+		//  CryptoStream cryptoStream = new CryptoStream(memoryStream,
+		//      cryptoProvider.CreateEncryptor(), CryptoStreamMode.Write);
+		//  StreamWriter writer = new StreamWriter(cryptoStream);
+		//  writer.Write(originalString);
+		//  writer.Flush();
+		//  cryptoStream.FlushFinalBlock();
+		//  writer.Flush();
+		//  return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
+		//}
+
+		//public static string Decrypt(string cryptedString)
+		//{
+		//  if (String.IsNullOrEmpty(cryptedString))
+		//  {
+		//    throw new ArgumentNullException
+		//       ("The string which needs to be decrypted can not be null.");
+		//  }
+		//  DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
+		//  MemoryStream memoryStream = new MemoryStream
+		//          (Convert.FromBase64String(cryptedString));
+		//  CryptoStream cryptoStream = new CryptoStream(memoryStream,
+		//      cryptoProvider.CreateDecryptor(), CryptoStreamMode.Read);
+		//  StreamReader reader = new StreamReader(cryptoStream);
+		//  return reader.ReadToEnd();
+		//}
+	}
+}
