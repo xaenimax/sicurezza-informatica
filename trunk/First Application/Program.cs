33e50f1b1f1c8f6a0623cfa8734a4558c9e89ad8
antoniopapa83 <antoniopapa83@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-02 09:14:25 +0000

    creazione classe parametri, ottimizzazione splitter file

--- trunk/First Application/Program.cs
+++ trunk/First Application/Program.cs
@@ -5,6 +5,30 @@ using System.Windows.Forms;
 
 namespace First_Application
 {
+
+    public class Parametri
+    {
+        public static int n_split ; // numero di sottofile in cui è splittato il file
+        public static int[] sts_1 ; // permutazione all'interno del sottofile
+
+        public Parametri()
+        {
+            n_split = 7;
+            sts_1 = new int[256] ;
+
+            for(int i = 0 ; i < sts_1.Length ; i++)
+            {
+                sts_1[i] = 0;
+            }
+
+
+            
+        }
+
+    }
+
+
+
     static class Program
     {
         /// <summary>
@@ -13,6 +37,7 @@ namespace First_Application
         [MTAThread]
         static void Main()
         {
+            Parametri param = new Parametri();
             Application.Run(new Form1());
         }
     }
