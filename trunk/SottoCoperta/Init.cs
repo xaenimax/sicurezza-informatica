ffd0dc83c146d781f754a06988c929f102ea0638
alex82rm <alex82rm@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-03 12:17:14 +0000

    Riformattati alcuni documenti

--- trunk/SottoCoperta/Init.cs
+++ trunk/SottoCoperta/Init.cs
@@ -7,64 +7,64 @@ using System.Windows.Forms;
 
 namespace SottoCoperta
 {
-    class Init
-    {
-        public static bool ctrl_firstStart() // controlla se l'applicazione è avviata per la prima volta
-        {
-            if (Directory.Exists(Parametri.cartella))
-            {
-                return false;
-                
-            }
-            return true;
-        }
+	class Init
+	{
+		public static bool ctrl_firstStart() // controlla se l'applicazione è avviata per la prima volta
+		{
+			if (Directory.Exists(Parametri.cartella))
+			{
+				return false;
 
-        public static void createConf() // crea directory e file di configurazione
-        {
-            DirectoryInfo dirInfo = new DirectoryInfo(Parametri.cartella);
-            dirInfo.Create();
-            dirInfo.Attributes |= FileAttributes.Hidden;
+			}
+			return true;
+		}
 
-            FileInfo f1 = new FileInfo(Parametri.fileconf);
-            f1.Create();
-            f1.Attributes |= FileAttributes.Hidden;
+		public static void createConf() // crea directory e file di configurazione
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(Parametri.cartella);
+			dirInfo.Create();
+			dirInfo.Attributes |= FileAttributes.Hidden;
 
-            FileInfo f2 = new FileInfo(Parametri.fileList);
-            f2.Create();
-            f2.Attributes |= FileAttributes.Hidden;
+			FileInfo f1 = new FileInfo(Parametri.fileconf);
+			f1.Create();
+			f1.Attributes |= FileAttributes.Hidden;
 
-            FileInfo f3 = new FileInfo(Parametri.fileIV);
-            f3.Create();
-            f3.Attributes |= FileAttributes.Hidden;
+			FileInfo f2 = new FileInfo(Parametri.fileList);
+			f2.Create();
+			f2.Attributes |= FileAttributes.Hidden;
 
-            fill_fileconf();
-        }
+			FileInfo f3 = new FileInfo(Parametri.fileIV);
+			f3.Create();
+			f3.Attributes |= FileAttributes.Hidden;
+
+			fill_fileconf();
+		}
 
 
-        public static void fill_fileconf() //genera il file conf
-        {
-            Application.Run(new SchermataIniziale_primoAvvio());
-        }
+		public static void fill_fileconf() //genera il file conf
+		{
+			Application.Run(new SchermataIniziale_primoAvvio());
+		}
 
-        public static void normalStart() // avvio normale dell'applicazione ( avvio diverso dal primo)
-        {
-            //Application.Run( new SchermataIniziale());
+		public static void normalStart() // avvio normale dell'applicazione ( avvio diverso dal primo)
+		{
+			//Application.Run( new SchermataIniziale());
 
-        }
+		}
 
-        public static void initStart() // funzione principale
-        {
-            if (ctrl_firstStart())
-            {
-                createConf();
-            }
-            else
-            {
-                normalStart();
-            }
+		public static void initStart() // funzione principale
+		{
+			if (ctrl_firstStart())
+			{
+				createConf();
+			}
+			else
+			{
+				normalStart();
+			}
 
-        }
+		}
 
-        
-    }
+
+	}
 }
--- trunk/SottoCoperta/Program.cs
+++ trunk/SottoCoperta/Program.cs
@@ -5,23 +5,18 @@ using System.Windows.Forms;
 
 namespace SottoCoperta
 {
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[MTAThread]
+		static void Main()
+		{
+			//Console.Write(
+			Parametri param = new Parametri();
+			Init.initStart();
 
-    
-
-
-
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [MTAThread]
-        static void Main()
-        {
-						//Console.Write(
-            Parametri param = new Parametri();
-            Init.initStart();
-            
-        }
-    }
+		}
+	}
 }
