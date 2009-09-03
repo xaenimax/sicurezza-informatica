f061c67e2396c944e0f988f31b6ee2df1532f85e
alex82rm <alex82rm@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-03 16:17:23 +0000

    aggiunta funziona ranomd in in Init.cs

--- SottoCoperta/Init.cs
+++ SottoCoperta/Init.cs
@@ -61,10 +61,40 @@ namespace SottoCoperta
 			else
 			{
 				normalStart();
+
+				int[] array = { 0, 0, 0 };
+
+				creaArrayRandom(ref array);
 			}
 
 		}
 
+		public static void creaArrayRandom(ref int[] vettore)
+		{
+			int valoreMassimo = (vettore.Length);
+			Random numeroRandom = new Random();
 
+			for (int i = 0; i < vettore.Length; i++)
+			{
+				//genero un numero random
+				int random = numeroRandom.Next(valoreMassimo);
+
+				bool b = true;
+
+				for (int j = 0; j < i; j++)
+				{
+					if (random == vettore[j])
+					{
+						b = false;
+						j = i;
+					}
+				}
+
+				if (b)
+					vettore[i] = random;
+				else
+					i--;
+			}
+		}
 	}
 }
