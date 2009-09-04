aab6bc3446841e906e9ab6473a19f13f5cfdeed8
alex82rm <alex82rm@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-04 09:03:35 +0000

    Tolta la funziona random da Init.cs e aggiuna una classe che conterrà tutti i metodi per creare "cose" random :)

---
+++ trunk/SottoCoperta/GeneratoreDiRandom.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SottoCoperta
+{
+
+	//Questa classe contiene e conterrà tutti i metodi che verranno utilizzati per generare
+	//numeri random, array random e stringhe random nonchè md5
+
+	class GeneratoreDiRandom
+	{
+		public static void creaArrayRandom(ref int[] vettore)
+		{
+			int valoreMassimo = (vettore.Length);
+			Random numeroRandom = new Random();
+
+			for (int i = 0; i < vettore.Length; i++)
+			{
+				//genero un numero random
+				int random = numeroRandom.Next(valoreMassimo);
+
+				bool b = true;
+
+				for (int j = 0; j < i; j++)
+				{
+					if (random == vettore[j])
+					{
+						b = false;
+						j = i;
+					}
+				}
+
+				if (b)
+					vettore[i] = random;
+				else
+					i--;
+			}
+		}
+	}
+}
