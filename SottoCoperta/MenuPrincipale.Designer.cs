5c40539655530a97e52cb94313e1cd6a6d1809b7
antoniopapa83 <antoniopapa83@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-04 14:29:34 +0000

    cazzo di budda ( un processo accede a gesù cristo)

--- SottoCoperta/MenuPrincipale.Designer.cs
+++ SottoCoperta/MenuPrincipale.Designer.cs
@@ -29,76 +29,76 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.mainMenu1 = new System.Windows.Forms.MainMenu();
-			this.menuItemVuoto = new System.Windows.Forms.MenuItem();
-			this.menuItem_esci = new System.Windows.Forms.MenuItem();
-			this.label1 = new System.Windows.Forms.Label();
-			this.button_recupera = new System.Windows.Forms.Button();
-			this.button_archivia = new System.Windows.Forms.Button();
-			this.button_nascondi = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// mainMenu1
-			// 
-			this.mainMenu1.MenuItems.Add(this.menuItemVuoto);
-			this.mainMenu1.MenuItems.Add(this.menuItem_esci);
-			// 
-			// menuItemVuoto
-			// 
-			this.menuItemVuoto.Text = "";
-			// 
-			// menuItem_esci
-			// 
-			this.menuItem_esci.Text = "Esci";
-			this.menuItem_esci.Click += new System.EventHandler(this.menuItem_esci_action);
-			// 
-			// label1
-			// 
-			this.label1.Font = new System.Drawing.Font("Tahoma", 10F, System.Drawing.FontStyle.Bold);
-			this.label1.Location = new System.Drawing.Point(29, 12);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(188, 20);
-			this.label1.Text = "Benvenuti in SottoCoperta";
-			// 
-			// button_recupera
-			// 
-			this.button_recupera.Location = new System.Drawing.Point(58, 83);
-			this.button_recupera.Name = "button_recupera";
-			this.button_recupera.Size = new System.Drawing.Size(120, 23);
-			this.button_recupera.TabIndex = 1;
-			this.button_recupera.Text = "Recupera un File";
-			this.button_recupera.Click += new System.EventHandler(this.button1_Click);
-			// 
-			// button_archivia
-			// 
-			this.button_archivia.Location = new System.Drawing.Point(58, 137);
-			this.button_archivia.Name = "button_archivia";
-			this.button_archivia.Size = new System.Drawing.Size(119, 24);
-			this.button_archivia.TabIndex = 2;
-			this.button_archivia.Text = "Archivia un file";
-			// 
-			// button_nascondi
-			// 
-			this.button_nascondi.Location = new System.Drawing.Point(58, 191);
-			this.button_nascondi.Name = "button_nascondi";
-			this.button_nascondi.Size = new System.Drawing.Size(119, 25);
-			this.button_nascondi.TabIndex = 3;
-			this.button_nascondi.Text = "Nascondi un file";
-			// 
-			// MenuPrincipale
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-			this.AutoScroll = true;
-			this.ClientSize = new System.Drawing.Size(240, 268);
-			this.Controls.Add(this.button_nascondi);
-			this.Controls.Add(this.button_archivia);
-			this.Controls.Add(this.button_recupera);
-			this.Controls.Add(this.label1);
-			this.Menu = this.mainMenu1;
-			this.Name = "MenuPrincipale";
-			this.Text = "SottoCoperta";
-			this.ResumeLayout(false);
+      this.mainMenu1 = new System.Windows.Forms.MainMenu();
+      this.menuItemVuoto = new System.Windows.Forms.MenuItem();
+      this.menuItem_esci = new System.Windows.Forms.MenuItem();
+      this.label1 = new System.Windows.Forms.Label();
+      this.button_recupera = new System.Windows.Forms.Button();
+      this.button_backup = new System.Windows.Forms.Button();
+      this.button_nascondi = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // mainMenu1
+      // 
+      this.mainMenu1.MenuItems.Add(this.menuItemVuoto);
+      this.mainMenu1.MenuItems.Add(this.menuItem_esci);
+      // 
+      // menuItemVuoto
+      // 
+      this.menuItemVuoto.Text = "";
+      // 
+      // menuItem_esci
+      // 
+      this.menuItem_esci.Text = "Esci";
+      this.menuItem_esci.Click += new System.EventHandler(this.menuItem_esci_action);
+      // 
+      // label1
+      // 
+      this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+      this.label1.Location = new System.Drawing.Point(68, 31);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(100, 22);
+      this.label1.Text = "Benvenuti!";
+      // 
+      // button_recupera
+      // 
+      this.button_recupera.Location = new System.Drawing.Point(58, 83);
+      this.button_recupera.Name = "button_recupera";
+      this.button_recupera.Size = new System.Drawing.Size(120, 23);
+      this.button_recupera.TabIndex = 1;
+      this.button_recupera.Text = "Recupera un File";
+      this.button_recupera.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // button_backup
+      // 
+      this.button_backup.Location = new System.Drawing.Point(58, 137);
+      this.button_backup.Name = "button_backup";
+      this.button_backup.Size = new System.Drawing.Size(119, 24);
+      this.button_backup.TabIndex = 2;
+      this.button_backup.Text = "Backup di un file";
+      // 
+      // button_nascondi
+      // 
+      this.button_nascondi.Location = new System.Drawing.Point(58, 191);
+      this.button_nascondi.Name = "button_nascondi";
+      this.button_nascondi.Size = new System.Drawing.Size(119, 25);
+      this.button_nascondi.TabIndex = 3;
+      this.button_nascondi.Text = "Nascondi un file";
+      // 
+      // MenuPrincipale
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+      this.AutoScroll = true;
+      this.ClientSize = new System.Drawing.Size(240, 268);
+      this.Controls.Add(this.button_nascondi);
+      this.Controls.Add(this.button_backup);
+      this.Controls.Add(this.button_recupera);
+      this.Controls.Add(this.label1);
+      this.Menu = this.mainMenu1;
+      this.Name = "MenuPrincipale";
+      this.Text = "SottoCoperta";
+      this.ResumeLayout(false);
 
 		}
 
@@ -108,7 +108,7 @@
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.MenuItem menuItem_esci;
 		private System.Windows.Forms.Button button_recupera;
-		private System.Windows.Forms.Button button_archivia;
+		private System.Windows.Forms.Button button_backup;
 		private System.Windows.Forms.Button button_nascondi;
 	}
 }
