47f7f1dfd95db33a496edb2dc361d7d3ce5c72ec
alex82rm <alex82rm@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-04 11:42:17 +0000

    Aggiunte le funzioni per calcolare l'md5, generare una stringa random dentro GeneratoreDiRandom.cs

--- trunk/SottoCoperta/GeneratoreDiRandom.cs
+++ trunk/SottoCoperta/GeneratoreDiRandom.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using System.Security.Cryptography;
 
 namespace SottoCoperta
 {
@@ -11,6 +12,9 @@ namespace SottoCoperta
 
 	class GeneratoreDiRandom
 	{
+
+		//Questo metodo prende in ingresso un array di int e in base alla dimensione
+		//genera n numeri random tutti diversi
 		public static void creaArrayRandom(ref int[] vettore)
 		{
 			int valoreMassimo = (vettore.Length);
@@ -38,5 +42,55 @@ namespace SottoCoperta
 					i--;
 			}
 		}
+
+
+		//Questa funziona calcola l'md5 di una stringa e ritorna una stringa con l'md5
+		public static string calcolaMd5(String stringaSuCuiCalcolareMd5)
+		{
+			String stringaMd5 = "";
+			Byte[] stringaConvertitaInByte;
+			Byte[] hashInByte;
+
+			ASCIIEncoding encoding = new ASCIIEncoding();
+
+			stringaConvertitaInByte = encoding.GetBytes(stringaSuCuiCalcolareMd5);
+
+			MD5 controlloreMd5 = MD5.Create();
+			hashInByte = controlloreMd5.ComputeHash(stringaConvertitaInByte);
+
+			for (int i = 0; i < hashInByte.Length; i++)
+				stringaMd5 += hashInByte[i].ToString("x2");
+
+			return stringaMd5;
+		}
+
+		//Questa funzione calcola l'md5 di un array di byte e ritorna l'md5 in byte
+		public static byte[] calcolaMd5(byte[] byteSuCuiCalcolareMd5)
+		{
+			Byte[] hashInByte;
+
+			MD5 controlloreMd5 = MD5.Create();
+			hashInByte = controlloreMd5.ComputeHash(byteSuCuiCalcolareMd5);
+
+			return hashInByte;
+		}
+
+		public static string generaStringaRandom(int numeroCaratteriDellaStringa)
+		{
+			char[] stringaRandom = new char[numeroCaratteriDellaStringa];
+			int numeroRandom;
+			Random generatoreRandom = new Random();
+
+			for (int i = 0; i < numeroCaratteriDellaStringa; i++)
+			{
+				numeroRandom = generatoreRandom.Next(0, 255); //secondo ascii sono le lettere da A a z (comprese maiuscole e minuscole)
+				stringaRandom[i] = Convert.ToChar(numeroRandom);
+			}
+
+			String stringa = new String(stringaRandom);
+
+			return stringa;
+		}
+
 	}
 }
