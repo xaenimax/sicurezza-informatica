ceef2c814f4c62cca7281102cbaee53220581a45
antoniopapa83 <antoniopapa83@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-04 13:28:33 +0000

    aggiunto rc4 non provato come tutto del resto (pacca fa il superiore)

---
+++ SottoCoperta/RC4.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace SottoCoperta
+{
+  class RC4
+  {
+
+    private const int keylength = 256;
+    private int[] s_box;
+    private int index_i;
+    private int index_j;
+    private byte[] key;
+
+    RC4()
+    {
+      s_box = new int[256];
+
+      for( int i = 0 ; i < s_box.Length ; i++)
+      {
+        s_box[i] = 0;
+      }
+
+      key = new byte[key_lengh];
+
+      index_i = 0;
+      index_j = 0;
+    }
+
+    public void init_RC4(byte[] chiave)
+    {
+      for (int i = 0; i < key.Length; i++)
+      {
+        key[i] = chiave[i % chiave.Length];
+      }
+
+      for (int i = 0; i < 256; i++)
+      {
+        s_box[i] = i;
+      }
+
+      int j = 0;
+
+      for (int i = 0; i < 256; i++)
+      {
+        j = (j + s_box[i] + (int)key[i % keylength]);
+        int temp = s_box[i];
+        s_box[i] = s_box[j];
+        s_box[j] = temp;
+      }
+
+    }
+
+
+    public void RC4_file(string percorso)
+    {
+      
+      FileStream fs = new FileStream(percorso, FileMode.Open, FileAccess.Read);
+
+      FileInfo fs_crypt = new FileInfo(percorso + "c");
+      fs_crypt.Create();
+      fs_crypt.Attributes |= FileAttributes.Hidden;
+      FileStream fc = new FileStream(percorso + "c" , FileMode.Open, FileAccess.Write);
+
+
+
+      int i = 0 ;
+      int j = 0 ;
+      for (int l = 0; l < fs.Length; i++)
+      {
+        byte in_byte = fs.ReadByte();
+
+        i = (i + 1) % 256 ;
+        j = (j + s_box[i]) % 256;
+
+        int temp = s_box[i];
+        s_box[i] = s_box[j];
+        s_box[j] = temp;
+
+        byte stream_byte = (byte) s_box[(s_box[i] + s_box[j]) % 256];
+
+        byte out_byte = in_byte ^ stream_byte;
+
+        fc.WriteByte(out_byte);
+
+      }
+
+      fs.Close;
+      fc.Close;
+
+    }
+
+  }
+}
+
+
--- SottoCoperta/SchermataIniziale_primoAvvio.cs
+++ SottoCoperta/SchermataIniziale_primoAvvio.cs
@@ -31,26 +31,39 @@ namespace SottoCoperta
 
 		private void avanti_menuItemAction(object sender, EventArgs e)
 		{
-			if (controllaPassword())
-			{
-				MessageBox.Show("Le password corrispondono!"); //TODO controllare anche la creazione del file conf
-                passwordUtente = password_textbox.Text;
+      if (controllaPassword())
+      {
+        MessageBox.Show("Le password corrispondono!"); //TODO controllare anche la creazione del file conf
+        passwordUtente = password_textbox.Text;
 
-                Parametri.Psw1 = passwordUtente;
+        Parametri.Psw1 = passwordUtente;
 
-                FileStream fs = new FileStream(Parametri.fileconf , FileMode.Open, FileAccess.Write );
-                
+        FileStream fs = new FileStream(Parametri.fileconf, FileMode.Open);
+        BinaryWriter bw = new BinaryWriter(fs);
 
-                FileSystem filesystem = new FileSystem();
+        creaArrayRandom(ref Parametri.sts_11);
+        creaArrayRandom(ref Parametri.sts_12);
 
-                //TODO generazione STS random;
+        for (int i = 0; i < Parametri.sts_11.Length; i++)
+        {
+          bw.Write(sts_11[i]);
+        }
 
-                //String s = Encrypt(password_textbox.Text);
-                //MessageBox.Show(s);
-                //s = Decrypt(s);
-                //MessageBox.Show(s); 
+        for (int i = 0; i < Parametri.sts_12.Length; i++)
+        {
+          bw.Write(sts_12[i]);
+        }
 
-			}
+        byte[] frase = new byte[16];
+        byte[] md5_frase = new byte[16];
+
+
+
+
+        FileSystem filesystem = new FileSystem();
+
+
+      }
 			else
 			{
 				MessageBox.Show("Attenzione, le password inserite non corrispondono");
@@ -69,39 +82,6 @@ namespace SottoCoperta
 				return true;
 		}
 
-		//public static string Encrypt(string originalString)
-		//{
-		//  if (String.IsNullOrEmpty(originalString))
-		//  {
-		//    throw new ArgumentNullException
-		//           ("The string which needs to be encrypted can not be null.");
-		//  }
-		//  DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
-		//  MemoryStream memoryStream = new MemoryStream();
-		//  CryptoStream cryptoStream = new CryptoStream(memoryStream,
-		//      cryptoProvider.CreateEncryptor(), CryptoStreamMode.Write);
-		//  StreamWriter writer = new StreamWriter(cryptoStream);
-		//  writer.Write(originalString);
-		//  writer.Flush();
-		//  cryptoStream.FlushFinalBlock();
-		//  writer.Flush();
-		//  return Convert.ToBase64String(memoryStream.GetBuffer(), 0, (int)memoryStream.Length);
-		//}
-
-		//public static string Decrypt(string cryptedString)
-		//{
-		//  if (String.IsNullOrEmpty(cryptedString))
-		//  {
-		//    throw new ArgumentNullException
-		//       ("The string which needs to be decrypted can not be null.");
-		//  }
-		//  DESCryptoServiceProvider cryptoProvider = new DESCryptoServiceProvider();
-		//  MemoryStream memoryStream = new MemoryStream
-		//          (Convert.FromBase64String(cryptedString));
-		//  CryptoStream cryptoStream = new CryptoStream(memoryStream,
-		//      cryptoProvider.CreateDecryptor(), CryptoStreamMode.Read);
-		//  StreamReader reader = new StreamReader(cryptoStream);
-		//  return reader.ReadToEnd();
-		//}
+		
 	}
 }
