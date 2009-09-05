1d78dc7a3a3ef56337f11962191fe1c64f69a6d0
alex82rm <alex82rm@cf10de0c-96e1-11de-a13a-71b910e68cc8>
2009-09-05 11:00:52 +0000

    Testato e Aggiunto RC4 FUNZIONANTE! YEEEEAAAH! :D

--- trunk/SottoCoperta/RC4.cs
+++ trunk/SottoCoperta/RC4.cs
@@ -8,14 +8,12 @@ namespace SottoCoperta
 {
   class RC4
   {
-
     private const int keylength = 256;
     private int[] s_box;
-    private int index_i;
-    private int index_j;
-    private byte[] key;
+		private byte[] key;
 
-    RC4()
+		//Inizializza la sbox
+    public RC4(byte[] chiave)
     {
       s_box = new int[256];
 
@@ -23,14 +21,12 @@ namespace SottoCoperta
       {
         s_box[i] = 0;
       }
-
-      key = new byte[key_lengh];
-
-      index_i = 0;
-      index_j = 0;
+      key = new byte[keylength];
+			inizializzaChiaveRC4(chiave);
     }
 
-    public void init_RC4(byte[] chiave)
+		//inizializza la sbox con la chiave inserita dall'utente
+    public void inizializzaChiaveRC4(byte[] chiave)
     {
       for (int i = 0; i < key.Length; i++)
       {
@@ -46,52 +42,77 @@ namespace SottoCoperta
 
       for (int i = 0; i < 256; i++)
       {
-        j = (j + s_box[i] + (int)key[i % keylength]);
+        j = (j + s_box[i] + (int)key[i % keylength]) % 256;
         int temp = s_box[i];
         s_box[i] = s_box[j];
         s_box[j] = temp;
       }
-
     }
 
-
-    public void RC4_file(string percorso)
+		//Prende in ingresso un percorso di un file e effettua lo XOR con lo stream RC4
+		//e risalva il file preso in ingresso con lo stesso nome.
+		//ATTENZIONE: cifra & decifra! :D
+    public void effettuaXORconKS(string percorso)
     {
-      
-      FileStream fs = new FileStream(percorso, FileMode.Open, FileAccess.Read);
+      FileStream inputFile = new FileStream(percorso, FileMode.Open, FileAccess.Read);
 
-      FileInfo fs_crypt = new FileInfo(percorso + "c");
-      fs_crypt.Create();
-      fs_crypt.Attributes |= FileAttributes.Hidden;
-      FileStream fc = new FileStream(percorso + "c" , FileMode.Open, FileAccess.Write);
+			FileInfo fileOriginale = new FileInfo(percorso);
+			//FileInfo fileCriptato = new FileInfo(fileOriginale.DirectoryName + "~" + fileOriginale.Name);
 
+			//FileStream fsFileCriptato = fileCriptato.Create();
+			FileInfo fileConTilde = creaFileConTilde(fileOriginale);
+			FileStream fsFileConTilde = new FileStream(fileConTilde.FullName, FileMode.Open, FileAccess.Write);
 
+      fileConTilde.Attributes |= FileAttributes.Hidden;
 
       int i = 0 ;
       int j = 0 ;
-      for (int l = 0; l < fs.Length; i++)
-      {
-        byte in_byte = fs.ReadByte();
 
+			//Scarto i primi 256byte dell'RC4
+      for (int l = 0; l < 256; l++)
+      {
         i = (i + 1) % 256 ;
         j = (j + s_box[i]) % 256;
 
         int temp = s_box[i];
         s_box[i] = s_box[j];
         s_box[j] = temp;
-
-        byte stream_byte = (byte) s_box[(s_box[i] + s_box[j]) % 256];
-
-        byte out_byte = in_byte ^ stream_byte;
-
-        fc.WriteByte(out_byte);
-
       }
-      fs.Close();
-      fc.Close();
-      File.Delete(percorso);
+
+			//Ora faccio lo XOR
+			for (int l = 0; l < inputFile.Length; l++)
+			{
+				byte in_byte = (byte)inputFile.ReadByte();
+
+				i = (i + 1) % 256;
+				j = (j + s_box[i]) % 256;
+
+				int temp = s_box[i];
+				s_box[i] = s_box[j];
+				s_box[j] = temp;
+
+				byte stream_byte = (byte)s_box[(s_box[i] + s_box[j]) % 256];
+				byte out_byte = (byte)(in_byte ^ stream_byte);
+
+				fsFileConTilde.WriteByte(out_byte);
+
+			}
+      inputFile.Close();
+			fileOriginale.Delete();
+      fsFileConTilde.Close();
+			File.Move(fileConTilde.FullName, fileOriginale.FullName);
     }
 
+		//Crea un nuovo file aggiungengo una tilde all'inizio del nome del file.
+		//Attenzione, per ripristinare il file senza la tilde il metodo non è stato creato
+		//perchè bisgona eliminare prima il file originale (tramite fileOriginale.Delete(); )
+		//e poi spostarlo tramite questa: File.Move(fileConTilde.FullName, fileOriginale.FullName);
+		public static FileInfo creaFileConTilde(FileInfo nomeFile) {
+			FileInfo nomeFileConTilde = new FileInfo(nomeFile.DirectoryName + "~" + nomeFile.Name);
+			FileStream fsNomeFileConTilde = nomeFileConTilde.Create();
+			fsNomeFileConTilde.Close();
+			return nomeFileConTilde;
+		}
   }
 }
 
